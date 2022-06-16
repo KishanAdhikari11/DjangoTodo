8dc6dec883737432d26f9616ff5311e173853ef9
kishanAd <adhikarykishan11@gmail.com>
2022-06-16 19:30:06 +0545

    secret key removed

--- Todo/settings.py
+++ Todo/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY ='django-insecure-hem&hm5d%)_09o#p%17wrue6$=ebo9vd2n6a2@+&nslnk@zn!*'
+SECRET_KEY =os.environ['Secret']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
