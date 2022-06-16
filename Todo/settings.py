9bb517b7bdd0bd4ceef66d09fd7ba2fb5776f78b
kishanAd <adhikarykishan11@gmail.com>
2022-06-16 19:19:49 +0545

    deployed heroku

--- Todo/settings.py
+++ Todo/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['Secret']
+SECRET_KEY ='django-insecure-hem&hm5d%)_09o#p%17wrue6$=ebo9vd2n6a2@+&nslnk@zn!*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["djangotodo7.herokuapp.com/","localhost:8000"]
+ALLOWED_HOSTS = ['djangotodo7.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
